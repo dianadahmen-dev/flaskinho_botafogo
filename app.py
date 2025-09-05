d212f732fa3aac86d01d3b9ffbf653f6f5621892
Diana Dahmen <diana_br@hotmail.de>
2025-09-05 16:46:21 -0300

    modificacoes

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello, World! Que legal'
+def home():
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return 'Sobre...'
+    return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  
+
+@app.route('/policies')
+def policies():
+    return render_template('policies.html')
+
+@app.route('/socialmedia')
+def socialmedia():
+    return render_template('socialmedia.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
